6bba103fee5000c77a82ae629a93d526ebaf63ed
habtegiorgiskassaye21 <habtegiorgiskassaye21@gmail.com>
2026-06-11 13:16:28 +0300

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -2,7 +2,10 @@ function toggleMenu(){
     const menu = document.querySelector(".menu-div");
     if(menu.style.display==="block"){
         menu.style.display="none"
+        document.querySelector(".menu-btn").innerHTML="&#9776";
+        
     }else{
         menu.style.display="block"
+        document.querySelector(".menu-btn").innerHTML="&times;";
     }
 }
